72061c8340ba1f569038e77f83bfa2ed1ddf79a4
petrubear <edison.martinez@gmail.com>
2020-06-14 20:19:35 -0500

    [java] feat: echo

--- java-vfs/src/main/java/emg/java/vfs/commands/Echo.java
+++ java-vfs/src/main/java/emg/java/vfs/commands/Echo.java
@@ -5,43 +5,44 @@ import emg.java.vfs.filesystem.State;
 import java.util.List;
 
 public class Echo extends Command {
-    private List<String> args;
+    private final String[] args;
 
     public Echo(List<String> args) {
-        this.args = args;
+        this.args = args.toArray(new String[0]);
     }
 
     @Override
     public State apply(State state) {
-        if (args.isEmpty()) {
+        if (args.length == 0) {
             return state;
-        } else if (args.size() == 1) {
-            return state.setMessage(args.get(0));
+        } else if (args.length == 1) {
+            return state.setMessage(args[0]);
         } else {
-            var operator = args.get(args.size() - 2);
-            var fileName = args.get(args.size() - 1);
-            var contents = createContent(args, args.size() - 2);
+            var operator = args[args.length - 2];
+            var fileName = args[args.length - 1];
+            var contents = createContent(args, args.length - 2);
 
             if (">>".equals(operator)) {
                 return doEcho(state, contents, fileName, true);
             } else if (">".equals(operator)) {
                 return doEcho(state, contents, fileName, false);
             } else {
-                return state.setMessage(createContent(args, args.size()));
+                return state.setMessage(createContent(args, args.length));
             }
         }
     }
 
-    private String createContent(List<String> args, int size) {
-        if (size == 0) {
-            return "";
+    private String createContent(String[] args, int topIndex) {
+        return createContentHelper(0, "", args, topIndex);
+    }
+
+    private String createContentHelper(int currentIndex, String accumulator, String[] args, int topIndex) {
+        if (currentIndex >= topIndex) {
+            return accumulator;
+        } else {
+            return createContentHelper(currentIndex + 1, String.format("%s %s", accumulator, args[currentIndex]),
+                args, topIndex);
         }
-        if (args.size() != size) {
-            for (int i = size; i <= args.size(); i++) {
-                args.remove(i - 1);
-            }
-        }
-        return String.join(" ", args);
     }
 
     private State doEcho(State state, String contents, String fileName, boolean append) {
