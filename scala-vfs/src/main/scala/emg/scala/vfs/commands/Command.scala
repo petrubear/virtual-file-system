165321caed86b1cb755fee74b51d4848321c6be9
petrubear <edison.martinez@gmail.com>
2020-06-16 17:37:13 -0500

    [scala] refactor: pattern matching

--- scala-vfs/src/main/scala/emg/scala/vfs/commands/Command.scala
+++ scala-vfs/src/main/scala/emg/scala/vfs/commands/Command.scala
@@ -2,8 +2,9 @@ package emg.scala.vfs.commands
 
 import emg.scala.vfs.filesystem.State
 
-trait Command {
-  def apply(state: State): State
+trait Command extends (State => State) {
+  // al extender de la funciona S => S ya no requiero definir apply
+  //def apply(state: State): State
 }
 
 object Command {
@@ -20,34 +21,32 @@ object Command {
 
   def incompleteCommand(name: String): Command = (state: State) => state.setMessage(s"${name}: Incomplete command!")
 
-
   def from(input: String): Command = {
     val tokens: Array[String] = input.split(" ")
     if (input.isEmpty || tokens.isEmpty) emptyCommand
-    else if (MKDIR.equals(tokens(0))) { //TODO esto puede hacerse con pattern matching?
-      if (tokens.length < 2) incompleteCommand(MKDIR)
-      else new Mkdir(tokens(1))
-    } else if (LS.equals(tokens(0))) {
-      new Ls
-    } else if (PWD.equals(tokens(0))) {
-      Pwd()
-    } else if (TOUCH.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(TOUCH)
-      else new Touch(tokens(1))
-    } else if (CD.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(CD)
-      else new Cd(tokens(1))
-    } else if (RM.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(RM)
-      else new Rm(tokens(1))
-    } else if (ECHO.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(ECHO)
-      else new Echo(tokens.tail)
-    } else if (CAT.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(CAT)
-      else new Cat(tokens(1))
+    else tokens(0) match {
+      case MKDIR =>
+        if (tokens.length < 2) incompleteCommand(MKDIR)
+        else new Mkdir(tokens(1))
+      case LS => new Ls
+      case PWD => Pwd()
+      case TOUCH =>
+        if (tokens.length < 2) incompleteCommand(TOUCH)
+        else new Touch(tokens(1))
+      case CD =>
+        if (tokens.length < 2) incompleteCommand(CD)
+        else new Cd(tokens(1))
+      case RM =>
+        if (tokens.length < 2) incompleteCommand(RM)
+        else new Rm(tokens(1))
+      case ECHO =>
+        if (tokens.length < 2) incompleteCommand(ECHO)
+        else new Echo(tokens.tail)
+      case CAT =>
+        if (tokens.length < 2) incompleteCommand(CAT)
+        else new Cat(tokens(1))
+      case _ => new UnknownCommand
     }
-    else new UnknownCommand
   }
 }
 
